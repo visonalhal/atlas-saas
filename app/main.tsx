f0e0e31f37eb1330db60e96e243099189f6597d5
halvin <halvin@nexttrucking.com>
2024-01-10 16:38:15 +0800

    chore: Upgrade dependencies

--- app/main.tsx
+++ app/main.tsx
@@ -2,12 +2,14 @@ import React from "react"
 import { createRoot } from "react-dom/client"
 import { createBrowserRouter, RouterProvider } from "react-router-dom"
 
+import App from "./App"
+
 import "./index.css"
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <div>Hello world!</div>,
+    element: <App />,
   },
 ])
 
